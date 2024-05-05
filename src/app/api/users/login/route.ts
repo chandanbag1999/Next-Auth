174d771625b33c79b207e28304cef3c44b2803ad
Chandan Bag <cbag67612@gmail.com>
2024-05-05 22:02:09 +0530

    complete user logout route

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -34,6 +34,7 @@ export async function POST(request: NextRequest){
         
         response.cookies.set("token", token, {
             httpOnly: true,
+            secure: true,
         })
 
         return response
