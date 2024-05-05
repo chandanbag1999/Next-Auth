18999f5f243986e0710507da022704d50a51a369
Chandan Bag <cbag67612@gmail.com>
2024-05-05 20:16:53 +0530

    complete signup route and send mail functionality

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,24 +1,42 @@
 import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
+import User from "@/models/userModel";
 
 export const sendEmail = async ({email, emailType, userId}: any) => {
     try {
-        // TODO: confugure mail for usage
+        const hashedToken = await bcryptjs.hash(userId.toString(), 10)
+
+        if (emailType === 'VERIFY') {
+            await User.findByIdAndUpdate(userId,
+                {
+                    verifyToken: hashedToken,
+                    verifyTokenExpiry: Date.now() + 3600000
+                })
+        } else if (emailType === 'RESET') {
+            await User.findByIdAndUpdate(userId,
+                {
+                    forgotPasswordToken: hashedToken,
+                    forgotPasswordTokenExpiry: Date.now() + 3600000
+                })
+        }
         
         const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, 
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
             auth: {
-              user: "maddison53@ethereal.email",
-              pass: "jn7jnAPss4f63QBp6D",
-            },
+              user: "d509078f9d70c7",
+              pass: "80dd5012f50219"
+            }
         });
 
         const mailOptions = {
             from: 'ckbag1998@gmail.com', 
             to: email,
             subject: emailType === 'VERIFY' ? 'Verify your email' : 'Reset your password',
-            html: "<b>Hello world?</b>", 
+            html: `<p>Click <a href='${process.env.DOMAIN}/verifyemail?token=${hashedToken}'>here</a> to ${emailType === 'VERIFY' ? 'verify your email' : 'reset your password'}
+            or copy and paste this link in your browser.
+            <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+            </p>`, 
         }
 
         const mailResponse = await transporter.sendMail(mailOptions);
