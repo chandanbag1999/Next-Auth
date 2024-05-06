a5f183fb84dee1a413d4d3ecb06432365ad5700c
Chandan Bag <cbag67612@gmail.com>
2024-05-06 17:07:55 +0530

    complete user get user details route

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,1 +1,23 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
 
+connectDB()
+
+export async function POST(request: NextRequest) {
+    // extract data from token
+    const userId = await getDataFromToken(request)
+    const user = await User.findOne({_id: userId}).select("-password")
+
+    if (!user) {
+        return NextResponse.json({error: "User not found"}, {status: 400})
+    }
+
+    return NextResponse.json({
+        message: "User fetched successfully",
+        success: true,
+        data: user,
+        status: 200
+    })
+}
